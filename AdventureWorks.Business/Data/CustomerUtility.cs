5c99a6ee0130839733dbd01e88cce4f9b1548906
Andrew Holzman <holzman.ad@gmail.com>
2016-03-01 11:23:03 -0500

    Added commented out section to retrieve Address info

--- AdventureWorks.Business/Data/CustomerUtility.cs
+++ AdventureWorks.Business/Data/CustomerUtility.cs
@@ -12,6 +12,11 @@ namespace AdventureWorks.Business.Data
     {
         public List<Customer> GetCustomer()
         {
+            List<Customer> Customers = new List<Customer>();
+            return Customers;
+        }
+        public List<Customer> GetCustomer(int CustomerID)
+        {
             List<Customer> colCustomers = new List<Customer>();
 
             //Connection
@@ -22,7 +27,8 @@ namespace AdventureWorks.Business.Data
 
             //Command
             SqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT * FROM SalesLT.Customer";
+            cmd.CommandText = "SELECT * FROM SalesLT.Customer WHERE CustomerID = @CustomerID";
+            cmd.Parameters.AddWithValue("@CustomerID", CustomerID);
 
             //Data Reader
             try
@@ -30,7 +36,7 @@ namespace AdventureWorks.Business.Data
                 conn.Open();
                 SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
 
-                while(reader.Read())
+                while (reader.Read())
                 {
                     Customer newCustomer = new Customer
                     {
@@ -55,12 +61,60 @@ namespace AdventureWorks.Business.Data
                 reader.Close();
             }
 
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 throw;
             }
 
             return colCustomers;
         }
+
+        //public List<Address> GetAddress(int AddressID)
+        //{
+        //    List<Address> colAddresses = new List<Address>();
+
+        //    //Connection
+        //    SqlConnection conn = new SqlConnection();
+
+        //    //Connection String
+        //    conn.ConnectionString = "Server=ANDYHOLZMAN;Database=AdventureWorks2012LT;Trusted_Connection=True;";
+
+        //    //Command
+        //    SqlCommand cmd = conn.CreateCommand();
+        //    cmd.CommandText = "SELECT * FROM SalesLT.Address WHERE AddressID = @AddressID";
+        //    cmd.Parameters.AddWithValue("@AddressID", AddressID);
+
+        //    //Data Reader
+        //    try
+        //    {
+        //        conn.Open();
+        //        SqlDataReader reader = cmd.ExecuteReader(System.Data.CommandBehavior.CloseConnection);
+
+        //        while (reader.Read())
+        //        {
+        //            Address newAddress = new Address
+        //            {
+        //                AddressID = (int)reader["AddressID"],
+        //                AddressLine1 = (string)reader["AddressLine1"],
+        //                AddressLine2 = (string)reader["AddressLine2"],
+        //                City = (string)reader["City"],
+        //                StateProvince = (string)reader["StateProvince"],
+        //                CountryRegion = (string)reader["CountryRegion"],
+        //                PostalCode = (string)reader["PostalCode"],
+        //                RowGuid = (Guid)reader["rowguid"],
+        //                ModifiedDate = (DateTime)reader["ModifiedDate"]
+        //            };
+        //            colAddresses.Add(newAddress);
+        //        }
+        //        reader.Close();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        throw;
+        //    }
+
+        //    return colAddresses;
+        //}
+
     }
 }
